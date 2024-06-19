aaacd8caa413a509c354d53e8f1cf376cf2245e1
sufyan <khan.sufyan.cs@gmail.com>
2024-06-19 08:42:17 -0400

    set up typescript lsp and add border to lsp hover window

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -7,4 +7,19 @@ return {
       },
     },
   },
+  {
+    "folke/noice.nvim",
+    opts = {
+      lsp = {
+        hover = {
+          -- Set not show a message if hover is not available
+          -- ex: shift+k on Typescript code
+          silent = true,
+        },
+      },
+      presets = {
+        lsp_doc_border = true,
+      },
+    },
+  },
 }
